3ccceac6d7594a98a85e88df2377e137582bc8f8
RajKKapadia <raajforyou@gmail.com>
2023-04-28 20:34:52 +0530

    predict success

--- predict.py
+++ predict.py
@@ -4,11 +4,11 @@ import cv2 as cv
 model = YOLO('runs/detect/train/weights/best.pt')
 
 result = model.predict(
-    source='datasets/images/train/IMG_2306_jpeg_jpg.rf.9bba6ce48724517b19474b85178391c1.jpg',
+    source='datasets/images/test/IMG_2301_jpeg_jpg.rf.2c19ae5efbd1f8611b5578125f001695.jpg',
     device=0
 )[0]
 
 print(result)
 
 result_plotted = result.plot(line_width=1)
-cv.imwrite('output.png')
+cv.imwrite('output.png', result_plotted)
